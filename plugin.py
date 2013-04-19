0c5ac746383932dde2e76fb496238bc51b9dd924
Daniel Miller <daniel@bonsaiviking.com>
2013-04-19 13:31:09 -0500

    Merge libdoc into url command

--- plugin.py
+++ plugin.py
@@ -43,6 +43,7 @@ import os
 import xml.sax
 from subprocess import Popen, PIPE
 import urllib
+import glob
 
 have_ndiff = True
 try:
@@ -118,6 +119,7 @@ class Ndoc(callbacks.Plugin):
             os.path.join(self.ndir, 'scripts'),
             os.path.join(self.ndir, 'nselib') )
         )
+        self.libs = map(lambda x: os.path.basename(x).split('.')[0], glob.glob(os.path.join(self.ndir, 'nselib', '*.lua*')))
         if have_pytags:
             self.tags = EtagFile()
             self.tags.parse_from_file(os.path.join(self.nsrc, 'TAGS'))
@@ -153,19 +155,31 @@ class Ndoc(callbacks.Plugin):
             irc.reply("Bad input")
     author = wrap(author, ['anything'])
 
-    def url(self, irc, msg, args, script):
-        """<script>
+    def liblist(self, irc, msg, args):
+        """
 
-        Returns the url of a script"""
-        m = reScript.match(script) 
-        if m:
-            script = "%s.nse" %( m.group('fname') )
-            try:
-                irc.reply(self.meta[script].url)
-            except KeyError:
-                irc.reply("Script not found")
+        Returns the list of NSE libraries."""
+        irc.replies(self.libs)
+    liblist = wrap(liblist, [])
+
+    def url(self, irc, msg, args, name):
+        """<name>
+
+        Returns a link to the NSEdoc page for <name>. <name> can be a script, library, or library.method."""
+        m = re.match(r'(?P<libname>\w+)(?:\.(?P<method>\w+)[\(\)]{0,2})?$', script)
+        if m and m.group('libname') in self.libs:
+            link = "http://nmap.org/nsedoc/lib/%s.html#%s" %(m.group('libname'), m.group('method') or '')
+            irc.reply( link.lower() )
         else:
-            irc.reply("Bad input")
+            m = reScript.match(script)
+            if m:
+                script = "%s.nse" %( m.group('fname') )
+                try:
+                    irc.reply(self.meta[script].url)
+                except KeyError:
+                    irc.reply("Script not found")
+            else:
+                irc.reply("Bad input")
     url = wrap(url, ['anything'])
 
     def usage(self, irc, msg, args, script):
@@ -463,18 +477,6 @@ class Ndoc(callbacks.Plugin):
         link = "https://encrypted.google.com/search?"
         irc.reply(link + urllib.urlencode({ 'q': "site:seclists.org inurl:nmap-dev %s" %(search)}))
     devlist = wrap(devlist, ['text'])
-
-    def libdoc(self, irc, msg, args, library):
-        """<library>
-
-        Returns a link to the NSEdoc page for <library>. If <library> is libname.method, then the link will go directly to the definition of method."""
-        m = re.match(r'(?P<libname>\w+)(?:\.(?P<method>\w+)[\(\)]{0,2})?$', library)
-        if m:
-            link = "http://nmap.org/nsedoc/lib/%s.html#%s" %(m.group('libname'), m.group('method') or '')
-            irc.reply( link.lower() )
-        else:
-            irc.reply("Invalid input")
-    libdoc = wrap(libdoc, ['something'])
 
 Class = Ndoc
 
