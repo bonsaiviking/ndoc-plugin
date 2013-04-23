39cfd1f5786b8e2294ebd68c56b9c4d02fcccd21
Daniel Miller <daniel@bonsaiviking.com>
2013-04-23 15:29:07 -0500

    New command luaterm

--- plugin.py
+++ plugin.py
@@ -139,6 +139,26 @@ class Ndoc(callbacks.Plugin):
                             self.errs[errstr].append(n)
                         else:
                             self.errs[errstr] = [n]
+        #TODO: try-catch
+        luaman = open(self.registryValue('luaManualTerms'),"r")
+        self.luaterms = {}
+        for term in luaman:
+            t = term.split("-")
+            if t[0] == "pdf":
+                self.luaterms[t[1]] = term
+            else:
+                self.luaterms[term] = term
+        luaman.close()
+
+    def luaterm(self, irc, msg, args, term):
+        """<term>
+
+        Returns a link to the Lua 5.2 manual section for <term>."""
+        if term in self.luaterms:
+            irc.reply("http://www.lua.org/manual/5.2/manual.html#{0}".format(self.luaterms[term]))
+        else:
+            irc.reply("No such term in Lua 5.2 manual: http://www.lua.org/manual/5.2/manual.html")
+    luaterm = wrap(luaterm, ['anything'])
 
     def author(self, irc, msg, args, script):
         """<script>
